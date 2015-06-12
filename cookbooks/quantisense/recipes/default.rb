3c6e6fe81446263659af6f493a25a884b01bc0a7
Rory Savage <rory.savage@epicor.com>
2015-06-12 13:28:20 -0400

    Adding initial QuantiSense chef-repo

---
+++ cookbooks/quantisense/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: quantisense 
+# Recipe:: default
+#
+# Copyright 2015, quantisense 
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package "telnet" do
+  action :upgrade
+end
